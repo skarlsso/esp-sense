75c6b0467b30919c6f0aff2bd7a047a1051fb8b5
Stefan Karlsson <stefan.karlsson@gmail.com>
2015-11-22 12:30:30 +0100

    Add init script

---
+++ src/init.lua
@@ -0,0 +1,8 @@
+-- Sleep 5 seconds before executing the script.
+-- This allows us to call tmr.stop() and prevent the script from running,
+-- which is good if something is broken in the script. :)
+tmr.alarm(0, 5000, 0, function()
+    local script = 'send-rth03-package.lua'
+    print("Running "..script)
+    dofile(script)
+end)
