3d265c8be4ebe1070106158ba72efa952c93d41f
Stefan Karlsson <stefan.karlsson@gmail.com>
2015-11-22 14:03:52 +0100

    Rename main script

--- src/init.lua
+++ src/init.lua
@@ -2,7 +2,7 @@
 -- This allows us to call tmr.stop() and prevent the script from running,
 -- which is good if something is broken in the script. :)
 tmr.alarm(0, 5000, 0, function()
-    local script = 'send-rth03-package.lua'
+    local script = 'main-rht03.lua'
     print("Running "..script)
     dofile(script)
 end)
